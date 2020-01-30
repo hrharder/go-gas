803448889a037d0aae504398aeb4d5c0b70fdac7
Henry Harder <henry@paradigm.market>
2020-01-30 11:07:45 -0500

    gas: add gas price suggester and use floats for JSON response

--- gas_price.go
+++ gas_price.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"math/big"
 	"net/http"
+	"sync"
+	"time"
 )
 
 // ETHGasStationURL is the API URL for the ETH Gas Station API.
@@ -14,6 +16,9 @@ const ETHGasStationURL = "https://ethgasstation.info/json/ethgasAPI.json"
 
 // GasPriority is a type alias for a string, with supported priorities included in this package.
 type GasPriority string
+
+// GasPriceSuggester is type alias  for a function that returns a reccomended gas price in base units for a given priority level.
+type GasPriceSuggester func(GasPriority) (*big.Int, error)
 
 const (
 	// GasPriorityFast is the recommended gas price for a transaction to be mined in less than 2 minutes.
@@ -37,7 +42,85 @@ func SuggestGasPrice(priority GasPriority) (*big.Int, error) {
 	if err != nil {
 		return nil, err
 	}
+	return parseSuggestedGasPrice(priority, prices)
+}
 
+// SuggestFastGasPrice is a helper method that calls SuggestGasPrice with GasPriorityFast
+func SuggestFastGasPrice() (*big.Int, error) {
+	return SuggestGasPrice(GasPriorityFast)
+}
+
+// NewGasPriceSuggester returns a function that can be used to either load a new gas price response, or use a cached
+// response if it is within the age range defined by maxResultAge.
+//
+// The returned function loads from the cache or pulls a new response if the stored result is older than maxResultAge.
+func NewGasPriceSuggester(maxResultAge time.Duration) (GasPriceSuggester, error) {
+	prices, err := loadGasPrices()
+	if err != nil {
+		return nil, err
+	}
+	m := gasPriceManager{
+		latestResponse: prices,
+		fetchedAt:      time.Now(),
+		maxResultAge:   maxResultAge,
+	}
+	return func(priority GasPriority) (*big.Int, error) {
+		return m.suggestCachedGasPrice(priority)
+	}, nil
+}
+
+type gasPriceManager struct {
+	latestResponse *ethGasStationResponse
+	fetchedAt      time.Time
+	maxResultAge   time.Duration
+
+	mux sync.Mutex
+}
+
+func (m *gasPriceManager) suggestCachedGasPrice(priority GasPriority) (*big.Int, error) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	// fetch new values if stored result is older than the maximum age
+	if time.Since(m.fetchedAt) > m.maxResultAge {
+		prices, err := loadGasPrices()
+		if err != nil {
+			return nil, err
+		}
+		m.latestResponse = prices
+		m.fetchedAt = time.Now()
+	}
+
+	return parseSuggestedGasPrice(priority, m.latestResponse)
+}
+
+// conversion factor to go from (gwei * 10) to wei
+// equal to: (raw / 10) => gwei => gwei * 1e9 => wei
+// simplifies to: raw * 1e8 => wei
+var conversionFactor = big.NewFloat(100000000)
+
+type ethGasStationResponse struct {
+	Fast    float64 `json:"fast"`
+	Fastest float64 `json:"fastest"`
+	SafeLow float64 `json:"safeLow"`
+	Average float64 `json:"average"`
+}
+
+func loadGasPrices() (*ethGasStationResponse, error) {
+	res, err := http.Get(ETHGasStationURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var body ethGasStationResponse
+	if err := json.NewDecoder(res.Body).Decode(&body); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func parseSuggestedGasPrice(priority GasPriority, prices *ethGasStationResponse) (*big.Int, error) {
 	switch priority {
 	case GasPriorityFast:
 		return parseGasPriceToWei(prices.Fast)
@@ -52,49 +135,10 @@ func SuggestGasPrice(priority GasPriority) (*big.Int, error) {
 	}
 }
 
-// SuggestFastGasPrice is a helper method that calls SuggestGasPrice with GasPriorityFast
-func SuggestFastGasPrice() (*big.Int, error) {
-	return SuggestGasPrice(GasPriorityFast)
-}
-
-// conversion factor to go from (gwei * 10) to wei
-// equal to: (raw / 10) => gwei => gwei * 1e9 => wei
-// simplifies to: raw * 1e8 => wei
-var conversionFactor = big.NewFloat(100000000)
-
-type ethGasStationResponse struct {
-	Fast    json.Number `json:"fast"`
-	Fastest json.Number `json:"fastest"`
-	SafeLow json.Number `json:"safeLow"`
-	Average json.Number `json:"average"`
-}
-
-func loadGasPrices() (*ethGasStationResponse, error) {
-	res, err := http.Get(ETHGasStationURL)
-	if err != nil {
-		return nil, err
-	}
-
-	dcr := json.NewDecoder(res.Body)
-	dcr.UseNumber()
-
-	var body ethGasStationResponse
-	if err := dcr.Decode(&body); err != nil {
-		return nil, err
-	}
-
-	return &body, nil
-}
-
 // convert eth gas station units to wei
 // (raw result / 10) * 1e9 = base units (wei)
-func parseGasPriceToWei(raw json.Number) (*big.Int, error) {
-	num, ok := new(big.Float).SetString(raw.String())
-	if !ok {
-		return nil, errors.New("eth: unable to parse float value")
-	}
-
-	gwei := new(big.Float).Mul(num, conversionFactor)
+func parseGasPriceToWei(raw float64) (*big.Int, error) {
+	gwei := new(big.Float).Mul(big.NewFloat(raw), conversionFactor)
 	if !gwei.IsInt() {
 		return nil, errors.New("eth: unable to represent gas price as integer")
 	}
